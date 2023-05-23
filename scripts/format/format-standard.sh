1086f40a32ed1f6932a6bed29eb8493ab5c797e1
LooXzy <looxzy@protonmail.com>
2023-05-23 13:47:03 +0200

    Create format-standard.sh

--- scripts/format/format-standard.sh
+++ scripts/format/format-standard.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+
+# Create new partition
+echo "n
+p
+1
+"
+
+# Change the partition to NTFS
+echo "t
+1
+7
+"
+
+# Save changes and exit
+echo "w
+" | fdisk /dev/sd[a-z]
+
+# Format partition to NTFS (Use mkntfs)
+mkntfs -Q /dev/sd[a-z]1
+
+# LOG ACTION
+date=$(date +"[%Y-%m-%d %H:%M:%S]")
+echo "$date [FORMAT][Standard] USB key formatted in NTFS" >> /sabu/logs/format.log
+
+# --- Script By SABU --- #
