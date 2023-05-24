4bf6adaa0069557df74f806b4038004063254ec0
LooXzy <looxzy@protonmail.com>
2023-05-24 18:17:40 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,10 @@ ipaddress=$(ip -br a | tail -n 1 | awk '{print $3}' | cut -d '/' -f 1)
 apt update
 apt upgrade -y
 
+# Add user SABU
+
+# Make permissions : user sabu
+
 # install package 
 
 # start gui
