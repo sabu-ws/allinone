40d4f5cda4e08d3b15d86c909888defefb9b23e9
LooXzy <looxzy@protonmail.com>
2023-05-24 11:48:49 +0200

    Update network-read.sh

--- scripts/network/network-read.sh
+++ scripts/network/network-read.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Retrieve network informations
+interface=$(ip -br a | awk '{print $1}' | grep -wv lo)
 address=$(grep address /etc/network/interfaces | cut -d ' ' -f2)
 netmask=$(grep netmask /etc/network/interfaces | cut -d ' ' -f2)
 gateway=$(grep gateway /etc/network/interfaces | cut -d ' ' -f2)
 nameserver=$(grep nameserver /etc/resolv.conf | cut -d ' ' -f2)
 
 # Display informations
-echo -e "$address\n$netmask\n$gateway\n$nameserver"
+echo -e "$interface\n$address\n$netmask\n$gateway\n$nameserver"
 
 # --- Script By SABU --- #
