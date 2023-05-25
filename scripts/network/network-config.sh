f32c8490a52aec31a809be95475c5dc84f7e4f01
LooXzy <looxzy@protonmail.com>
2023-05-25 12:42:22 +0200

    Update network-config.sh

--- scripts/network/network-config.sh
+++ scripts/network/network-config.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+path=$(pwd)
+
 # RETRIEVE NETWORK INFORMATION
-interface=$(jq '.network | .interface' /SABU/config/config.json  | tr -d '"')
-address=$(jq '.network | .ip' /SABU/config/config.json  | tr -d '"')
-netmask=$(jq '.network | .netmask' /SABU/config/config.json  | tr -d '"')
-gateway=$(jq '.network | .gateway' /SABU/config/config.json  | tr -d '"')
-nameserver1=$(jq '.network | .dns1' /SABU/config/config.json  | tr -d '"')
-nameserver2=$(jq '.network | .dns2' /SABU/config/config.json  | tr -d '"')
+interface=$(jq '.network | .interface' $path/../../config/config.json  | tr -d '"')
+address=$(jq '.network | .ip' $path/../../config/config.json  | tr -d '"')
+netmask=$(jq '.network | .netmask' $path/../../config/config.json  | tr -d '"')
+gateway=$(jq '.network | .gateway' $path/../../config/config.json  | tr -d '"')
+nameserver1=$(jq '.network | .dns1' $path/../../config/config.json  | tr -d '"')
+nameserver2=$(jq '.network | .dns2' $path/../../config/config.json  | tr -d '"')
 
 # Retrieve the line number of the network interface
 lignes=$(grep -n $interface /etc/network/interfaces | tail -n +2 | cut -d ':' -f1)
@@ -23,6 +25,6 @@ systemctl restart networking.service
 
 # LOG ACTION
 date=$(date +"[%Y-%m-%d %H:%M:%S]")
-echo "$date [NETWORK][Interface] The network interface '$interface' has been configured" >> /sabu/logs/network.log
+echo "$date [NETWORK][Interface] The network interface $interface has been configured" >> $path/../../logs/network.log
 
 # --- Script By SABU --- #
