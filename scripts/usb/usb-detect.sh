0ba8409e8c99bfa496d9ba8db8375fe000009ec5
LooXzy <looxzy@protonmail.com>
2023-05-25 12:49:30 +0200

    Update usb-detect.sh

--- scripts/usb/usb-detect.sh
+++ scripts/usb/usb-detect.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+path=$(pwd)
+
 # LOG ACTION
 date=$(date +"[%Y-%m-%d %H:%M:%S]")
-echo "$date [USB][Detect] USB key detected" >> /sabu/logs/usb.log
+echo "$date [USB][Detect] USB key detected" >> $path/../../logs/usb.log
 
 # --- Script By SABU --- #
