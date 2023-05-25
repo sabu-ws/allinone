5f3f01b290f8a3862dffc8e1ec653c4a130e87ce
LooXzy <looxzy@protonmail.com>
2023-05-25 11:19:23 +0200

    Update network-config.sh

--- scripts/network/network-config.sh
+++ scripts/network/network-config.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
-# Arguments
-## $1 = address
-## $2 = netmask
-## $3 = gateway
-## $4 = domain-server1
-## $5 = domain-server2
-
-# RETRIEVE NETWORK INTERFACE
-interface=$(ip -br a | awk '{print $1}' | grep -wv lo)
+# RETRIEVE NETWORK INFORMATION
+interface=$(jq '.network | .interface' /SABU/config/config.json  | tr -d '"')
+address=$(jq '.network | .ip' /SABU/config/config.json  | tr -d '"')
+netmask=$(jq '.network | .netmask' /SABU/config/config.json  | tr -d '"')
+gateway=$(jq '.network | .gateway' /SABU/config/config.json  | tr -d '"')
+nameserver1=$(jq '.network | .dns1' /SABU/config/config.json  | tr -d '"')
+nameserver2=$(jq '.network | .dns2' /SABU/config/config.json  | tr -d '"')
 
 # Retrieve the line number of the network interface
 lignes=$(grep -n $interface /etc/network/interfaces | tail -n +2 | cut -d ':' -f1)
 
 # Change the network information in relation to the arguments
-sed -i "$(($lignes+1))c\address $1" /etc/network/interfaces
-sed -i "$(($lignes+2))c\netmask $2" /etc/network/interfaces
-sed -i "$(($lignes+3))c\gateway $3" /etc/network/interfaces
-sed -i "1c\nameserver $4" /etc/resolv.conf
-sed -i "2c\nameserver $5" /etc/resolv.conf
+sed -i "$(($lignes+1))c\address $address" /etc/network/interfaces
+sed -i "$(($lignes+2))c\netmask $netmask" /etc/network/interfaces
+sed -i "$(($lignes+3))c\gateway $gateway" /etc/network/interfaces
+sed -i "1c\nameserver $nameserver1" /etc/resolv.conf
+sed -i "2c\nameserver $nameserver2" /etc/resolv.conf
 
 # Restart the network configuration
 systemctl restart networking.service
 
 # LOG ACTION
 date=$(date +"[%Y-%m-%d %H:%M:%S]")
-echo "$date [NETWORK][Interface] The network interface has been configured" >> /sabu/logs/network.log
+echo "$date [NETWORK][Interface] The network interface '$interface' has been configured" >> /sabu/logs/network.log
 
 # --- Script By SABU --- #
