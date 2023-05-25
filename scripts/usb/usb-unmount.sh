4617e6eec84e922dcaf34595a1046c1dff0c3c3b
LooXzy <looxzy@protonmail.com>
2023-05-25 12:50:24 +0200

    Update usb-unmount.sh

--- scripts/usb/usb-unmount.sh
+++ scripts/usb/usb-unmount.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+path=$(pwd)
+
 # UNMOUNT USB STICK
 sudo umount /mnt/usb
 
 # LOG ACTION
 date=$(date +"[%Y-%m-%d %H:%M:%S]")
-echo "$date [USB][Umount] USB key unmounted" >> /sabu/logs/usb.log
+echo "$date [USB][Umount] USB key unmounted" >> $path/../../logs/usb.log
 
 # --- Script By SABU --- #
