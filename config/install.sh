1057f530a42a202bd32b69fc698e741d70ac74f6
LooXzy <looxzy@protonmail.com>
2023-05-26 14:20:59 +0200

    Update setup.sh

--- config/install.sh
+++ config/install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-
 # crontab script update at 2 AM
+ echo "0 2 * * * sh /sabu/config/update.sh" >> crontab -e 
 
 # install package
-apt install 
+apt install parted mkfs ntfs-3g dd yara clamav exiftools ipcalc iptables -y
 
 # start script iptables prod
+sh /sabu/scripts/network/network-iptables-prod.sh
 
 # Reboot SABU
+reboot
