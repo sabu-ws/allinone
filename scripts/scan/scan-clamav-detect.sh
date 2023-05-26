a7610d5b24785c55dfb9f8b48356058ce0b2469f
Minvinea <sabu.bsi@pronton.me>
2023-05-26 11:03:02 +0200

    Fix script

--- scripts/scan/scan-clamav-detect.sh
+++ scripts/scan/scan-clamav-detect.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-DATE=$(/bin/date "+%d%m%Y%H%M%S")
-PATH="/mnt/usb/"
+DATE=$(date "+%d%m%Y%H%M%S")
+SCAN_PATH="/mnt/usb/"
 LOG_PATH="/sabu/logs/clamav"
 
-/usr/bin/echo $LOG_NAME
-
 # CLAMSCAN
-#/usr/bin/echo "$LOG_PATH/clamscan-detect-$DATE.log"
-#/usr/bin/clamscan -r $PATH > "$LOG_PATH/clamscan-detect-$DATE.log"
+clamscan -r $SCAN_PATH > "$LOG_PATH/clamscan-detect-$DATE.log"
 
-#/usr/bin/cat "$LOG_PATH/clamscan-detect-$DATE.log"
-DETECT_FILE=$(/usr/bin/cat "$LOG_PATH/clamscan-detect-25052023095241.log" | /usr/bin/grep FOUND | /usr/bin/tr -d ':' | /usr/bin/cut -d' ' -f 1,2)
-
-
-echo "--- SCAN REPORT ---"
-echo "Date: $(/bin/date "+%d/%m/%Y %H:%M:%S")"
-echo "Infected files: $(/usr/bin/cat "/sabu/logs/clamav/clamscan-detect-25052023095241.log" | /usr/bin/grep "Infected files" | /usr/bin/cut -d' ' -f3)"
-echo "Files:"
-echo "$(/usr/bin/cat "$LOG_PATH/clamscan-detect-25052023095241.log" | /usr/bin/grep FOUND | /usr/bin/tr -d ':' | /usr/bin/cut -d' ' -f 1,2)"
+date=$(date +"[%Y-%m-%d %H:%M:%S]")
+echo "$date [Scan][Clamav] New scan available clamscan-detect-$DATE.log" >> $LOG_PATH/../scan.log
