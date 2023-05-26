94b6ccc4fd6cf5b388910f200053c8386f30a946
LooXzy <looxzy@protonmail.com>
2023-05-26 11:05:24 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,18 +3,32 @@
 # RETRIEVE NETWORK IP
 ipaddress=$(ip -br a | tail -n 1 | awk '{print $3}' | cut -d '/' -f 1)
 
-# Update packages
-apt update
-apt upgrade -y
+# Setup workspace
+mkdir /sabu
+cp * /sabu
 
-# Add user SABU
+# Install packages 
+apt install sudo python3 python3-pip ipcalc iptables iptables-persistent 
 
-# Make permissions : user sabu
+sleep 3
 
-# install package 
+# Adduser/permission SABU
+adduser sabu --shell=/bin/false --no-create-home
+echo "sabu ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+chown sabu:sabu /sabu
+chmod +x -R /sabu/scripts
 
-# start gui
+# Install python requirements
+pip3 install -r ./gui/requirements.txt
 
-# end
+sleep 3
+
+# Start gui
+systemctl start sabu.service
+
+# End
 echo "Setup completed !"
-echo "Open browser and visit : https://$ipaddress/admin
+echo "Open browser and visit : https://$ipaddress/admin"
+
+# Remove tmp folder
+rm -rf ../SABU
