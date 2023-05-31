8c579c5589837cb2d5173cce4af2c7ac39b5cb55
LooXzy <looxzy@protonmail.com>
2023-05-31 23:49:57 +0200

    Edit update.sh

--- config/update.sh
+++ config/update.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# LOG ACTION
+date=$(date +"[%Y-%m-%d %H:%M:%S]")
+echo "$date [SABU] The update script has been executed" >> /sabu/logs/sabu.log
+
 
 # iptables maintenance
-
+sh /sabu/scripts/network/network-iptables-maintenance.sh
 
 # update sabu
-
+apt update && apt upgrade -y
 
 # iptables prod
+sh /sabu/scripts/network/network-iptables-prod.sh
 
 # reboot
+reboot
+
+# --- Script By SABU --- #
