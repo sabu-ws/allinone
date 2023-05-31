df6cf2e25e416bd6e46e499e5623eff7c500c3d1
Tranos <56634165+tranosgames@users.noreply.github.com>
2023-05-31 22:10:37 +0200

    Add first connection webpage

--- gui/server.py
+++ gui/server.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify, render_template, request, url_for , g , flash,
 from werkzeug.utils import secure_filename
 from flask_socketio import SocketIO, emit
 import eventlet
-#import pandas as pd
+
 import hashlib
 import re
 import json
@@ -32,14 +32,16 @@ CONFIG_PATH = "../config"
 app.config['SECRET_KEY'] = ''.join(random.choices(string.ascii_letters + string.digits, k=30))
 app.config['UPLOAD_FOLDER'] = ROOT_PATH
 hasScan = False
+first_con = True
+
 
 def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if g.log == False:
-            return redirect(url_for('admin', next=request.url))
-        return f(*args, **kwargs)
-    return decorated_function
+	@wraps(f)
+	def decorated_function(*args, **kwargs):
+		if g.log == False:
+			return redirect(url_for('admin', next=request.url))
+		return f(*args, **kwargs)
+	return decorated_function
 
 def detectUSB(f):
 	@wraps(f)
@@ -53,14 +55,24 @@ def detectUSB(f):
 	return decorated_function
 
 def ifscan(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        global hasScan
-        if hasScan == False:
-            return redirect(url_for('scan'))
-        return f(*args, **kwargs)
-    return decorated_function
+	@wraps(f)
+	def decorated_function(*args, **kwargs):
+		global hasScan
+		if hasScan == False:
+			return redirect(url_for('scan'))
+		return f(*args, **kwargs)
+	return decorated_function
 
+def first(f):
+	@wraps(f)
+	def decorated_function(*args, **kwargs):
+		file = open("/sabu/gui/static/config.json")
+		js = json.load(file)
+		if "mdp" in js:
+			if js["mdp"] == "":
+				return redirect(url_for('first_connection'))
+		return f(*args, **kwargs)
+	return decorated_function
 
 def ret_mode(mode_num):
 	return mode_num.replace("7","rwx").replace("6","rw-").replace("5","r-x").replace("4","r--").replace("3","-wx").replace("2","-w-").replace("1","--x").replace("0","---")
@@ -74,15 +86,18 @@ def sizeof_fmt(num, suffix="B"):
 
 
 @app.route("/")
+@first
 def index():
 	return render_template("main.html")
 
 @app.route("/scan")
+@first
 @detectUSB
 def scan_page():
 	return render_template("scan_page.html")
 
 @app.route("/scan/simple")
+@first
 @detectUSB
 def scan_simple():
 	return render_template("scan_simple.html")
@@ -100,6 +115,7 @@ def rec():
 			break
 
 @app.route("/scan/advanced")
+@first
 @detectUSB
 def scan_advanced():
 	return render_template("scan_advanced.html")
@@ -127,11 +143,13 @@ def rec():
 			break
 
 @app.route("/format")
+@first
 @detectUSB
 def format():
 	return render_template("format_page.html")
 
 @app.route("/format/simple")
+@first
 @detectUSB
 def format_simple():
 	return render_template("format_simple.html")
@@ -148,6 +166,7 @@ def rec():
 			break
 
 @app.route("/format/advanced")
+@first
 @detectUSB
 def format_advanced():
 	return render_template("format_advanced.html")
@@ -167,6 +186,7 @@ def rec():
 @app.route("/browser/<path:MasterListDir>")
 @app.route("/browser/")
 @app.route("/browser")
+@first
 @detectUSB
 def browser(MasterListDir=""):
 	joining = os.path.join(ROOT_PATH,MasterListDir)
@@ -204,6 +224,7 @@ def browser(MasterListDir=""):
 
 @app.route("/download/<path:MasterListDir>")
 @app.route("/download")
+@first
 @ifscan
 @detectUSB
 def download(MasterListDir=""):
@@ -229,6 +250,7 @@ def download(MasterListDir=""):
 
 @app.route("/delete/<path:MasterListDir>")
 @app.route("/delete")
+@first
 @detectUSB
 def delete(MasterListDir=""):
 	path=ROOT_PATH+"/"+MasterListDir
@@ -253,6 +275,7 @@ def delete(MasterListDir=""):
 
 @app.route("/info/<path:MasterListDir>")
 @app.route("/info")
+@first
 @detectUSB
 def info(MasterListDir=""):
 	path=ROOT_PATH+"/"+MasterListDir
@@ -262,6 +285,7 @@ def info(MasterListDir=""):
 	return render_template("sh_file.html",info=sub)
 
 @app.route("/sendd",methods=['POST'])
+@first
 @ifscan
 @detectUSB
 def sendd():
@@ -309,6 +333,7 @@ def sendd():
 		return redirect(url_for("browser"))
 
 @app.route("/admin",methods=('GET','POST'))
+@first
 def admin():
 	if session.get('loggedin') == True:
 		return redirect(url_for("admin_config"))
@@ -329,6 +354,7 @@ def admin():
 
 
 @app.route("/admin/config",methods=['GET', 'POST'])
+@first
 @login_required
 def admin_config():
 	if session.get('loggedin') == True:
@@ -377,13 +403,77 @@ def admin_config():
 	else:
 		return redirect(url_for('admin_config'))
 
+
+@app.route("/first",methods=["POST","GET"])
+def first_connection():
+	global first_con
+	if first_con == True:
+		info_ip = subprocess.Popen(f"{SCRIPT_PATH}/network/network-read.sh".split(),stdout=subprocess.PIPE).communicate()[0].decode().split("\n")
+		if request.method=="POST":
+			# return request.form
+
+			if ("interface" in request.form and "ip" in request.form and "netmask" in request.form and "gateway" in request.form and "dns1" in request.form and "password" in request.form):
+				must_match_ip = r"^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\.(?!$)|$)){4}$"
+
+				# Min 12 char, 1 number, 1 uppercase, 1 lowercase,1 spécial char
+				must_match_pwd = r"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[\*\.!@$%^\&\(\)\{\}\[\]:;<>,\.\?\/~_\+-=\|]).{12,}$"
+				if request.form["dns2"] != "":
+					dns2 = request.form["dns2"]
+				else:
+					dns2 = "9.9.9.9"
+				if re.search(must_match_ip, request.form["ip"]) and re.search(must_match_ip, request.form["netmask"]) and re.search(must_match_ip, request.form["gateway"]) and re.search(must_match_ip, request.form["dns1"]) and re.search(must_match_ip, request.form["dns2"]) and re.search(must_match_pwd, request.form["password"]):
+					interface = request.form["interface"]
+					ip = request.form["ip"]
+					netmask = request.form["netmask"]
+					gateway = request.form["gateway"]
+					dns1 = request.form["dns1"]
+					mdp=request.form["password"]
+					dico_network = {"interface": interface, "ip": ip, "netmask": netmask, "gateway": gateway, "dns1": dns1, "dns2": dns2}
+					dico_category_network = {"network": dico_network}
+					json_config = open(CONFIG_PATH+"/config.json", "w")
+					json.dump(dico_category_network, json_config)
+					subprocess.Popen(f"{SCRIPT_PATH}/network/network-config.sh".split())
+					file_r = open("static/config.json","r")
+					js = json.load(file_r)
+					file_r.close()
+					file_w=open("static/config.json","w")
+					encrypt=hashlib.sha512(mdp.encode()).hexdigest()
+					js['mdp']=encrypt
+					json.dump(js, file_w)
+					file_w.close()
+					first_con = False
+				else:
+					flash("Some informations was incorrect")
+					return redirect(url_for("first_connection"))
+			else:
+				flash("Some informations missing !") 
+				return redirect(url_for("first_connection"))
+		elif request.method=="GET":
+			interface=info_ip[0]
+			ip=info_ip[1]
+			netmask=info_ip[2]
+			gateway=info_ip[3]
+			dns1=info_ip[4]
+			dns2=info_ip[5]
+		else:
+			return "404"
+		return render_template("first.html",interface=interface,ip=ip,netmask=netmask,gateway=gateway,dns1=dns1,dns2=dns2)
+	elif first_con == False:
+		return redirect("/")
+	else:
+		return redirect("/404")
+
+	return ""
+
 @app.route("/logout")
+@first
 @login_required
 def logout():
 	session["loggedin"]=False
 	return redirect(url_for('index'))
 
 @app.route("/nobody")
+@first
 def nobody():
 	value = "No key found"
 	return render_template("nobody.html",value=value)		
