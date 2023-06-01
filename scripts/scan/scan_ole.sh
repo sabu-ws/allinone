7c6cfade4c14ee215d392c3745ad53468fe0dba5
Tranos <56634165+tranosgames@users.noreply.github.com>
2023-06-01 20:45:24 +0200

    Add files via upload

---
+++ scripts/scan/scan_ole.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+pathUSB='/mnt/usb'
+LOG_PATH='/sabu/logs/'
+outfile=$(date +%s)
+
+for i in $(find $pathUSB -type f)
+do
+        format=$(python3 /home/debian/oletest/oletools/oleid.py $i | grep "Container format" | cut -d'|' -f2 | tr -d ' ')
+        if [ "$format" = "OLE" ];
+        then
+                macro=$(python3 /home/debian/oletest/oletools/oleid.py $i | grep -E "VBA Macros|XML Macros" | cut -d'|' -f2 | tr -d ' ')
+                if [ "$macro" = "Yes,suspicious" ];
+                then
+                        echo "$i is suspicious" >> $LOG_PATH/ole-$DATE.log
+                fi;
+        fi;
+done
+date=$(date +"[%Y-%m-%d %H:%M:%S]")
+echo "$date [Scan][OLE] New scan available ole-$DATE.log" >> $LOG_PATH/scan.log
+echo "ole-$DATE.log"
+
+# --- Script By SABU --- #
