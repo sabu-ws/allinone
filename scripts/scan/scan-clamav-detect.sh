30c32fe30b238204d650abb662d55607e200f3d6
Tranos <56634165+tranosgames@users.noreply.github.com>
2023-06-02 23:14:02 +0200

    Update scan-clamav-detect.sh

--- scripts/scan/scan-clamav-detect.sh
+++ scripts/scan/scan-clamav-detect.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-DATE=$(date "+%d%m%Y%H%M%S")
+DATE=$(date +%s)
 SCAN_PATH="/mnt/usb/"
-LOG_PATH="/sabu/logs/clamav"
+LOG_PATH="/sabu/logs/scan/clamav"
 
 # CLAMSCAN
 clamscan -r $SCAN_PATH > "$LOG_PATH/clamscan-detect-$DATE.log"
 
 date=$(date +"[%Y-%m-%d %H:%M:%S]")
-echo "$date [Scan][Clamav] New scan available clamscan-detect-$DATE.log" >> $LOG_PATH/../scan.log
-echo "$date [Scan][Clamav] New scan available clamscan-detect-$DATE.log" > $LOG_PATH/../last-scan.log
+echo "$date [Scan][Clamav] New scan available clamscan-detect-$DATE.log" >> $LOG_PATH/../../scan.log
+echo "clamscan-detect-$DATE.log" > $LOG_PATH/last-scan.log
 
 # --- Script By SABU --- #
