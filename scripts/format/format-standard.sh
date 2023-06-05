183c5367f78a981b5c33b7abea8ac891f3820f8f
LooXzy <looxzy@protonmail.com>
2023-06-05 13:53:59 +0200

    Fix permissions

--- scripts/format/format-standard.sh
+++ scripts/format/format-standard.sh
@@ -4,19 +4,19 @@
 device='/dev/sd[a-z]'
 
 # Delete all partitions on the USB key
-echo 'yes' | sudo parted -s $device mklabel msdos
+echo 'yes' | parted -s $device mklabel msdos
 
 # Create a new primary partition occupying all available space
-echo 'yes' | sudo parted -s -a optimal $device mkpart primary ntfs 0% 100%
+echo 'yes' | parted -s -a optimal $device mkpart primary ntfs 0% 100%
 
 # Update partition table
-sudo partprobe $device
+partprobe $device
 
 # Wait for changes to take effect
 sleep 1
 
 # Format partition to NTFS
-sudo mkfs.ntfs -f ${device}1
+mkfs.ntfs -f ${device}1
 
 # LOG ACTION
 date=$(date +"[%Y-%m-%d %H:%M:%S]")
