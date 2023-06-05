9e16308cc99353d9f64a9da4e8ac41cc715e95c6
LooXzy <looxzy@protonmail.com>
2023-06-05 13:41:50 +0200

    Fix permissions

--- config/update.sh
+++ config/update.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # iptables maintenance
-sh /sabu/scripts/network/network-iptables-maintenance.sh
+sudo sh /sabu/scripts/network/network-iptables-maintenance.sh
 
 # update sabu
-apt update && apt upgrade -y
+sudo apt update && sudo apt upgrade -y
 
 # update clamav databases
-systemctl stop clamav-freshclam.service
-freshclam -d
+sudo systemctl stop clamav-freshclam.service
+sudo freshclam -d
 sleep 3
-systemctl start clamav-freshclam.service
+sudo systemctl start clamav-freshclam.service
 
 # iptables prod
-sh /sabu/scripts/network/network-iptables-prod.sh
+sudo sh /sabu/scripts/network/network-iptables-prod.sh
 
 # LOG ACTION
 date=$(date +"[%Y-%m-%d %H:%M:%S]")
 echo "$date [SABU] The update script has been executed" >> /sabu/logs/sabu.log
 
 # reboot
-reboot
+sudo reboot
 
 # --- Script By SABU --- #
