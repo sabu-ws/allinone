0f1317d1ac7194c1a8c0ddcf28371ae9588e41e9
LooXzy <looxzy@protonmail.com>
2023-06-07 00:38:00 +0200

    Fix syntax

--- scripts/scan/scan-clamav-detect.sh
+++ scripts/scan/scan-clamav-detect.sh
@@ -8,7 +8,7 @@ LOG_PATH="/sabu/logs/scan/clamav"
 clamscan -r $SCAN_PATH > "$LOG_PATH/clamscan-detect-$DATE.log"
 
 date=$(date +"[%Y-%m-%d %H:%M:%S]")
-echo "$date [Scan][Clamav] New scan available clamscan-detect-$DATE.log" >> $LOG_PATH/../../scan.log
+echo "$date [SCAN][Clamav] New scan available clamscan-detect-$DATE.log" >> $LOG_PATH/../../scan.log
 echo "clamscan-detect-$DATE.log" > $LOG_PATH/last-scan.log
 
 # --- Script By SABU --- #
