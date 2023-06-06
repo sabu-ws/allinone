32adbd972d2013b3662dd0411c3781e25610b617
LooXzy <looxzy@protonmail.com>
2023-06-06 23:02:04 +0200

    Update check.sh

--- config/check.sh
+++ config/check.sh
@@ -5,7 +5,7 @@ date=$(date +"[%Y-%m-%d %H:%M:%S]")
 echo "$date [SABU] Your device has been started" >> /sabu/logs/sabu.log
 
 # Check if sabu.service is running
-status=$(systemctl status sabu.service | grep Active | awk '{ print $3}' | tr -d '()')
+status=$(systemctl status sabu.service | grep Active | awk '{print $3}' | tr -d '()')
 
 if [ $status="running" ]
 then
