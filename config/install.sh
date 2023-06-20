2ea87e2523603a1910ed92b597473998a46ae72b
Tranos <56634165+tranosgames@users.noreply.github.com>
2023-06-20 13:11:48 +0200

    Update install.sh

--- config/install.sh
+++ config/install.sh
@@ -43,4 +43,6 @@ sudo sh /sabu/scripts/network/network-nftables-prod.sh
 date=$(date +"[%Y-%m-%d %H:%M:%S]")
 echo "$date [SABU] The installation script has been executed successfully" >> /sabu/logs/sabu.log
 
+sudo reboot
+
 # --- Script By SABU --- #
