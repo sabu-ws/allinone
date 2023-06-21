71b982cf3baa7da23f0367335fdd9088494b71ee
LooXzy <looxzy@protonmail.com>
2023-06-21 18:13:13 +0200

    add admin_page

--- gui/server.py
+++ gui/server.py
@@ -578,7 +578,7 @@ def admin_config():
 			dns2=info_ip[5]
 		else:
 			return "404"
-		return render_template("config.html",interface=interface,ip=ip,netmask=netmask,gateway=gateway,dns1=dns1,dns2=dns2)
+		return render_template("admin_config.html",interface=interface,ip=ip,netmask=netmask,gateway=gateway,dns1=dns1,dns2=dns2)
 	else:
 		return redirect(url_for('admin_config'))
 
