3ffee14f13ce4bb7d1711db51e27e27160e2b1d5
Tranos <56634165+tranosgames@users.noreply.github.com>
2023-06-22 09:39:08 +0200

    Update and add updating script

--- config/update.sh
+++ config/update.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-# iptables maintenance
-sudo sh /sabu/scripts/network/network-nftables-maintenance.sh
+# Absolute path to scripts
+SCRIPT_LAUNCH_UPDATE="/sabu/config/launch_update.sh"
 
-# update sabu
-sudo apt update && sudo apt upgrade -y
+# Target user
+USER="sabu"
 
-# update clamav databases
-sudo systemctl stop clamav-freshclam.service
-sudo freshclam -d
-sleep 3
-sudo systemctl start clamav-freshclam.service
+# Extract content from user crontab
+crontab -u "$USER" -l > /tmp/crontab_tmp
 
-# iptables prod
-sudo sh /sabu/scripts/network/network-nftables-prod.sh
+# Add new cron job to crontab content
+echo "@reboot sh $SCRIPT_LAUNCH_UPDATE" >> /tmp/crontab_tmp
+
+# Update user crontab with changed content
+crontab -u "$USER" /tmp/crontab_tmp
+
+# Delete temporary file
+rm /tmp/crontab_tmp
 
 # LOG ACTION
 date=$(date +"[%Y-%m-%d %H:%M:%S]")
-echo "$date [SABU] The update script has been executed" >> /sabu/logs/sabu.log
+echo "$date [SABU] The before update script was set" >> /sabu/logs/sabu.log
 
-# reboot
 sudo reboot
 
 # --- Script By SABU --- #
