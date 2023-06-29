c3700766b28ac7b73c4d8b094bad6a35c37067c9
LooXzy <looxzy@protonmail.com>
2023-06-30 00:09:13 +0200

    Remove beta

--- scripts/network/network-hostname.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Change hosts in /etc/hosts
-get_hostname=$(hostname)
-sudo sed -i "s/127\.0\.1\.1\t\w*$/127\.0\.1\.1\t${get_hostname}/g" /etc/hosts
-
-# --- Script By SABU --- #
--- scripts/usb/usb-detect.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# LOG ACTION
-date=$(date +"[%Y-%m-%d %H:%M:%S]")
-echo "$date [USB][Detect] USB key detected" >> /sabu/logs/usb.log
-
-# --- Script By SABU --- #
